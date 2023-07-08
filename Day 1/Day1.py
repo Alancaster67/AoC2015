9b03d92b72b2c3a7b1771f0aab09308b1397869a
Austin Lancaster <alancaster67@gmail.com>
2023-07-07 22:31:03 -0600

    Refactored Day1.py by defining the decoder funtction from the numpy vectorize method.

--- Day 1/Day1.py
+++ Day 1/Day1.py
@@ -1,9 +1,11 @@
 #%%
 import numpy as np
 with  open('input.txt','r') as f:
-    instructions = f.read()
+    instructions = np.array(list(f.read()))
 
-floors = np.vectorize({"(":1,")":-1}.get)(np.array(list(instructions)))
+decoder = np.vectorize({"(":1,")":-1}.get)
+floors = decoder(instructions)
 
 final_floor = floors[-1]
 first_basement = np.where(np.cumsum(floors)==-1)[0][0] + 1
+# %%
